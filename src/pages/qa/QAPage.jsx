c5434d4cc926e3bd39269bc34f8f3b095cb33912
YeongseoYoo <youngseo1314@sookmyung.ac.kr>
2024-02-23 15:02:21 +0900

    QA페이지 구현

--- src/pages/qa/QAPage.jsx
+++ src/pages/qa/QAPage.jsx
@@ -3,9 +3,36 @@ import CustomNavbar from '../../components/common/nav/CustomNavbar';
 
 const QAPage = () => {
     return (
-        <>
-            <Container>QA PAGE</Container>
-        </>
+        <Container>
+            <h3>지식투자 리스트</h3>
+
+            <ListGroup>
+                {loading ? (
+                    <ListGroup.Item>Loading QAs...</ListGroup.Item>
+                ) : (
+                    qas.map((qa, index) => (
+                        <Link
+                            key={qa._id}
+                            to={`/qa/${qa._id}`} // Assuming there's a route for individual QA pages
+                            className="text-decoration-none"
+                        >
+                            <ListGroup.Item action className="d-flex justify-content-between align-items-start">
+                                <div className="ms-2 me-auto text-truncate">
+                                    <div className="fw-bold">{index + 1} | {qa.title}</div>
+                                    <div>{qa.content}</div>
+                                </div>
+                                <div className="d-flex flex-column justify-content-center align-items-end">
+                                    <Badge bg="primary" pill>
+                                        {qa.commentCount} {/* Assuming commentCount is available */}
+                                    </Badge>
+                                    <div>{qa.author.nickname}</div>
+                                </div>
+                            </ListGroup.Item>
+                        </Link>
+                    ))
+                )}
+            </ListGroup>
+        </Container>
     );
 };
 
