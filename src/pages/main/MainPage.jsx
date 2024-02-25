8f554fbdc58e9cb2e9a934f14820e15c3688a23c
chaeheonjeong <cogjs3210@naver.com>
2024-02-26 08:36:29 +0900

    feat: boj problem

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,11 +1,13 @@
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
+import RecommendProblems from "../../components/beakjoon/RecommendProblems";
 
 const MainPage = () => {
-    return (
-        <>
-            <Container>메인화면</Container>
-        </>
-    );
+  return (
+    <>
+      <Container>메인화면</Container>
+      <RecommendProblems />
+    </>
+  );
 };
 
 export default MainPage;
