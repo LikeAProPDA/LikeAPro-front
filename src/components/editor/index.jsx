257d65d0e640482adb18aab44c3d63a0831b5c6a
YeongseoYoo <youngseo1314@sookmyung.ac.kr>
2024-02-26 16:52:01 +0900

    editor 내용 markdown 형식으로 띄우기

---
+++ src/components/editor/index.jsx
@@ -0,0 +1,2 @@
+export { default as Editor, default } from './editor'
+export { default as Viewer } from './viewer'
--- src/pages/qa/QADetailPage.jsx
+++ src/pages/qa/QADetailPage.jsx
@@ -1,27 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Container, Button, Card } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import qaApi from '../../lib/apis/qaApi';
-import commentApi from '../../lib/apis/commentApi'; // Import commentApi
-// Toast UI Editor
-import { Editor } from '@toast-ui/react-editor';
+import commentApi from '../../lib/apis/commentApi';
+import { Editor } from '../../components/editor';
+import ReactMarkdown from "react-markdown";
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
+
 
 const QADetailPage = () => {
   const { id } = useParams();
   const [qa, setQA] = useState({});
   const [comments, setComments] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [editorContent, setEditorContent] = useState(""); // 에디터의 내용을 state로 관리합니다.
-  const [title, setTitle] = useState(""); // 제목을 state로 관리합니다.
-  const [showEditor, setShowEditor] = useState(false); // 에디터를 보여줄지 여부를 관리하는 상태
+  const [showEditor, setShowEditor] = useState(false);
+  const editorRef = useRef(null);
 
   useEffect(() => {
     const fetchQA = async () => {
       try {
         setLoading(true);
-        // Fetch QA data based on id
         const data = await qaApi.getaQA(id);
         setQA(data.data);
         setLoading(false);
@@ -33,7 +32,6 @@ const QADetailPage = () => {
 
     const fetchComments = async () => {
       try {
-        // Fetch comments for QA based on id
         const data = await commentApi.getCommentsForQA(id);
         setComments(data.result);
       } catch (error) {
@@ -45,60 +43,91 @@ const QADetailPage = () => {
     fetchComments();
   }, [id]);
 
-  const handlePostQA = async () => {
+
+  const handleEditorChange = (value) => {
+    setEditorContent(value);
+  };
+
+  const handleSave = async () => {
+    const markdownContent = editorRef.current.getInstance().getMarkdown();
+    console.log(markdownContent);
     try {
-      // postQA 함수를 사용하여 새로운 QA를 등록합니다.
-      await qaApi.postQA(title, editorContent, "User", false);
-      // 등록 후에 화면을 새로고침하여 새로운 QA가 표시되도록 합니다.
-      window.location.reload();
+      await commentApi.uploadCommentForQA(id, markdownContent);
+      const data = await commentApi.getCommentsForQA(id);
+      setComments(data.result);
     } catch (error) {
-      console.error("Error posting QA:", error);
+      console.error("Error saving comment:", error);
     }
   };
 
   return (
     <Container>
+      <br />
       {loading ? (
         <div>Loading...</div>
       ) : (
         <>
-          <div>
-            <Card bg="info" text="white" style={{ margin: '20px 0', padding: '20px', borderRadius: '15px' }}>
-              <h2>{qa.qa.title}</h2>
-              <p>{qa.qa.content}</p>
-            </Card>
+          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Button onClick={() => setShowEditor(true)} style={{ borderColor: 'blue', color: 'blue', backgroundColor: 'transparent' }}>글 작성하기</Button>
           </div>
 
-          <Button onClick={() => setShowEditor(true)}>글 작성하기</Button>
+          <Card
+            style={{
+              margin: '20px 0',
+              height: "200px",
+              padding: '20px',
+              borderRadius: '15px',
+              backgroundColor: '#E3EDFF',
+              boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+            }}
+          >
+            <h2>{qa.qa.title}</h2>
+            <p>{qa.qa.content}</p>
+          </Card>
 
           {showEditor && (
             <>
               <h3>Add your knowledge!</h3>
-              <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="제목을 입력하세요" />
               <Editor
-                height="450px"
+                ref={editorRef}
+                height="400px"
                 placeholder="Please Enter Text."
                 initialValue="내용을 입력해주세요."
                 theme="dark"
                 previewStyle='vertical'
-                onChange={(value) => setEditorContent(value)} // 에디터의 내용이 변경될 때마다 state를 업데이트합니다.
+                onChange={handleEditorChange}
               />
-              <Button onClick={handlePostQA}>등록</Button>
+              
+              <Button onClick={handleSave} style={{ float: 'right', borderColor: 'blue', color: 'blue', backgroundColor: 'transparent' }}>Save</Button>
             </>
           )}
 
           <h3>Comments</h3>
           {comments.map((comment, index) => (
-            <Card key={index} bg="info" text="white" style={{ margin: '10px 0', padding: '10px', borderRadius: '15px' }}>
-              <p>{comment.content}</p>
+            <Card
+              key={index}
+              style={{
+                margin: '10px 0',
+                padding: '10px',
+                borderRadius: '15px',
+                backgroundColor: '#fff',
+                boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+              }}
+            >
+              {/* markdown 형식의 내용을 HTML로 렌더링 */}
+              <ReactMarkdown components={{
+                a: (props) => <a target="_blank" style={{ color: "red" }} {...props} />,
+              }}>
+                {comment.content}
+              </ReactMarkdown>
               <p>작성자: {comment.user.nickname}</p>
             </Card>
           ))}
         </>
       )}
-
     </Container>
   );
 };
 
 export default QADetailPage;
+
