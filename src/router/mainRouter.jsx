fce96631ce20eda03e8c907b1bf2c42134bcf14a
chaeheonjeong <cogjs3210@naver.com>
2024-02-27 09:13:41 +0900

    feat: Add beakjoon, score page

--- src/router/mainRouter.jsx
+++ src/router/mainRouter.jsx
@@ -1,37 +1,43 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Layout from '../pages/Layout';
-import MainPage from '../pages/main/MainPage';
-import QAPage from '../pages/qa/QAPage';
-import LoginPage from '../pages/login/LoginPage';
-import SignUpPage from '../pages/signUp/SignUpPage';
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../pages/Layout";
+import MainPage from "../pages/main/MainPage";
+import QAPage from "../pages/qa/QAPage";
+import LoginPage from "../pages/login/LoginPage";
+import SignUpPage from "../pages/signUp/SignUpPage";
+import RankingPage from "../pages/ranking/RankingPage";
 
 export const mainRouter = [
-    {
-        path: '',
-        element: <Layout />,
-        children: [
-            {
-                path: '/',
-                element: <MainPage />,
-                index: true,
-            },
-            {
-                path: '/qas',
-                element: <QAPage />,
-                index: true,
-            },
-            {
-                path: '/login',
-                element: <LoginPage />,
-                index: true,
-            },
-            {
-                path: '/sign-up',
-                element: <SignUpPage />,
-                index: true,
-            },
-        ],
-    },
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <MainPage />,
+        index: true,
+      },
+      {
+        path: "/qas",
+        element: <QAPage />,
+        index: true,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+        index: true,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUpPage />,
+        index: true,
+      },
+      {
+        path: "/ranking",
+        element: <RankingPage />,
+        index: true,
+      },
+    ],
+  },
 ];
 
 const router = createBrowserRouter(mainRouter);
